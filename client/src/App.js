51d9818596a755b1dbfcda133676c6607ff5d048
beckyxde <beckyxde@users.noreply.github.com>
2019-03-07 19:52:41 +0100

    updates

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+import { Switch, Router } from "react-router-dom";
+import Home from "./components/Home";
+import Collections from "./components/Collections";
+import Signup from "./components/Signup";
+import axios from "axios";
+
+class App extends Component {
+  constructor() {
+    super();
+    //  this.state = {
+    //   data: data
+    // };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
