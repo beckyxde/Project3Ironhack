2cb5e92dc9583a898448dcb07215a534b2774542
Liska Crofts <yscrofts@hotmail.com>
2019-03-11 00:04:21 +0100

    rendering signup component in app, installed axios in frontend, worked on signup component

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+// import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
-const Signup = () => {
-  return (
-    <div>
-      <h1>Signup</h1>
-    </div>
-  );
-};
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  handleFormSubmit = (event) => {
+
+    event.preventDefault();
+    const username = this.state.username;
+    const password = this.state.password;
+
+    Axios.post('http://localhost:/api/signup', { username: username, password: password }).then((response) => {
+      this.setState({
+        username: "",
+        password: "",
+      })
+    })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Signup</h1>
+        <form>
+
+        </form>
+      </div>
+    )
+  }
+
+}
 
 export default Signup;
