50e28b9c8b79b58939e200678acb16d16f1c44ad
beckyxde <beckyxde@users.noreply.github.com>
2019-03-12 21:44:05 +0100

    huh?

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // import { Link } from 'react-router-dom';
-import Axios from 'axios';
+import Axios from "axios";
 
 class Signup extends Component {
   constructor(props) {
@@ -8,46 +8,69 @@ class Signup extends Component {
     this.state = {
       name: "",
       email: "",
-      password: ""
+      password: "",
+      redirect: false
     };
   }
 
-  handleFormSubmit = (event) => {
-
+  handleFormSubmit = event => {
     event.preventDefault();
     const email = this.state.email;
     const password = this.state.password;
     const name = this.state.name;
 
-    Axios.post('http://localhost:5000/api/signup', { name: name, email: email, password: password }).then((response) => {
-      this.setState({
-        name: "",
-        email: "",
-        password: ""
-      })
+    Axios.post("http://localhost:5000/api/signup", {
+      name: name,
+      email: email,
+      password: password
     })
-      .catch(error => console.log(error))
-  }
+      .then(response => {
+        this.setState({
+          name: "",
+          email: "",
+          password: ""
+        });
+      })
+      .catch(error => console.log(error));
+  };
 
-  handleChange = (event) => {
+  handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
-  }
+  };
 
   render() {
     return (
       <div>
         <h1>Signup</h1>
         <form onSubmit={this.handleFormSubmit}>
-          <input type="text" name="name" placeholder="Your Name" value={this.state.name} onChange={e => this.handleChange(e)} ></input>
-          <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={e => this.handleChange(e)} ></input>
-          <input name="password" placeholder="password" value={this.state.password} onChange={e => this.handleChange(e)} ></input>
-          <button type="submit" name="submit">Submit!</button>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            value={this.state.name}
+            onChange={e => this.handleChange(e)}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={e => this.handleChange(e)}
+          />
+          <input
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={e => this.handleChange(e)}
+          />
+          <button type="submit" name="submit">
+            Submit!
+          </button>
         </form>
       </div>
-    )
+    );
   }
-
 }
 
 export default Signup;
