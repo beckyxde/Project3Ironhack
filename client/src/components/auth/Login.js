01cb7a8f11c4f1a78e999c44a3c43ca8f734211c
Liska Crofts <yscrofts@hotmail.com>
2019-03-16 12:06:45 +0100

    just fiddlin'

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -28,4 +28,38 @@ class Login extends Component {
     const email = this.state.email;
     const password = this.state.password;
   }
+
+  Axios.
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleFormSubmit}>
+          {this.renderRedirect()}
+          <input
+            type="text"
+            name="email"
+            placeholder="email"
+            value={this.state.email}
+            onChange={e => this.handleChange(e)}
+          />
+          <input
+            name="password"
+            placeholder="password"
+            value={this.state.password}
+            onChange={e => this.handleChange(e)}
+          />
+          <button type="submit" name="submit" onClick={this.setRedirect}>
+            Login!
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
